a27ff0898515f1777e9cb41cb009fbd25fafbcc4
mk10969 <mk10969@gmail.com>
2020-11-15 04:56:30 +0900

    go and rust

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		// nothing
+	}
+
+	fmt.Println("start ping !")
+
+}
+
+func connectToMysql() {
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	username := os.Getenv("MYSQL_USERNAME")
+	password := os.Getenv("MYSQL_PASSWORD")
+	database := os.Getenv("MYSQL_DATABASE")
+
+}
